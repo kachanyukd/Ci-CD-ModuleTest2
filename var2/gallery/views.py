cd3001aefc3f913131abfe430417c777c4ccec54
kachanyukd <kachanyukdima@gmail.com>
2025-05-23 16:10:55 +0300

    add views

--- var2/gallery/views.py
+++ var2/gallery/views.py
@@ -1,1 +1,13 @@
 from django.shortcuts import render, get_object_or_404
+from .models import Category, Image
+
+def gallery_view(request):
+    categories = Category.objects.all()
+
+    return render(request, 'gallery.html', {"categories" : categories})
+
+
+def image_detail(request, pk):
+    image = Image.objects.filter(id=pk).first()
+
+    return render(request, 'image_detail.html', {"image" : image})
